ae88e39f2ac5d748011053c61ebb3d474c41f4a4
Taylor Savage <tjsavage@google.com>
2015-12-06 17:20:10 -0800

    added some logic to close the server

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -12,4 +12,17 @@ var server = app.listen(config.frontend.port, function() {
   var port = server.address().port;
 
   console.log("application-server-manager frontend listening at http://%s:%s", host, port);
+});
+
+process.on('exit', function() {
+  server.close();
 })
+
+process.on('uncaughtException', function() {
+  console.log("uncaught exception");
+  server.close();
+});
+
+process.on('SIGTERM', function() {
+  server.close();
+});
