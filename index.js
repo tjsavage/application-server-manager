6b5d1acbfb200aa1528c7695606b85a2757a8ba5
Taylor Savage <tjsavage@google.com>
2015-12-06 17:51:14 -0800

    fixed bug with change of github handling

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var exec = require('child_process').exec;
 var config = require('./config.json');
 
 require('./src/application-proxy');
-require('./src/github-deployment');
 require('./src/frontend');
 
 config.applications.forEach(function(app) {
